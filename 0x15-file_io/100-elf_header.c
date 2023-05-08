3f654683b9906bff1061372c32a5581c462b1d43
ANABUIKE2 <elementlinks2@gmail.com>
2023-05-08 13:19:08 -0700

    0x15-file_io

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,23 +1,3 @@
-/*
- * File: 100-elf_header.c
- * Auth: Anabuike Samson
- */
-		break;
-	case ET_REL:
-		printf("REL (Relocatable file)\n");
-		break;
-	case ET_EXEC:
-		printf("EXEC (Executable file)\n");
-		break;
-	case ET_DYN:
-		printf("DYN (Shared object file)\n");
-		break;
-	case ET_CORE:
-		printf("CORE (Core file)\n");
-		break;
-	default:
-		printf("<unknown: %x>\n", e_type);
-	}
 }
 
 /**
